4da4d368ef3fec61abe450f397bdf5eb3f3066bd
RobertPinson <pinson_rob@yahoo.co.uk>
2013-11-05 22:05:57 +0000

    Split bill view components added

--- MyFirstApp/MyFirstApp/src/com/example/myfirstapp/MainActivity.java
+++ MyFirstApp/MyFirstApp/src/com/example/myfirstapp/MainActivity.java
@@ -1,37 +1,77 @@
 package com.example.myfirstapp;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+	public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
 	@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		final ListView listview = (ListView) findViewById(R.id.listview_bills);
+	    String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+	        "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+	        "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
+	        "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
+	        "Android", "iPhone", "WindowsMobile" };
 
+	    final ArrayList<String> list = new ArrayList<String>();
+	    for (int i = 0; i < values.length; ++i) {
+	      list.add(values[i]);
+	    }
+	    final ArrayAdapter adapter = new ArrayAdapter(this,
+	        android.R.layout.simple_list_item_1, list);
+	    listview.setAdapter(adapter);
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
-    public void sendMessage(View view)
-    {
-    	Intent intent = new Intent(this, DisplayMessageActivity.class);
-    	EditText editText = (EditText)findViewById(R.id.edit_message);
-    	String message = editText.getText().toString();
-    	intent.putExtra(EXTRA_MESSAGE, message);
-    	startActivity(intent);
-    }
-    
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	public void splitMinus(View view) {
+		try {
+			TextView splitBy = (TextView) findViewById(R.id.edit_splitby);
+			int split = Integer.parseInt(splitBy.getText().toString());
+
+			if (split > 0) {
+				split--;
+			}
+
+			splitBy.setText(String.format("%02d", split));
+		} catch (NumberFormatException nfe) {
+			System.out.println("Could not parse " + nfe);
+		}
+	}
+
+	public void splitPlus(View view) {
+		try {
+			TextView splitBy = (TextView) findViewById(R.id.edit_splitby);
+			int split = Integer.parseInt(splitBy.getText().toString());
+
+			if (split < 50) {
+				split++;
+			}
+
+			splitBy.setText(String.format("%02d", split));
+		} catch (NumberFormatException nfe) {
+			System.out.println("Could not parse " + nfe);
+		}
+	}
+
 }
