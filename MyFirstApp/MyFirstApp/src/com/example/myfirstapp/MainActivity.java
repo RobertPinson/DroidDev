a4c24458d64802c47fb7e6c23757367e512160b6
RobertPinson <pinson_rob@yahoo.co.uk>
2013-11-10 07:42:52 +0000

    icon added rotate state issue fixed

--- MyFirstApp/MyFirstApp/src/com/example/myfirstapp/MainActivity.java
+++ MyFirstApp/MyFirstApp/src/com/example/myfirstapp/MainActivity.java
@@ -5,11 +5,9 @@ import java.util.Currency;
 import java.util.Locale;
 import java.math.BigDecimal;
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -29,6 +27,31 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	@Override
+	protected void onRestoreInstanceState(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onRestoreInstanceState(savedInstanceState);
+		
+		int splitValue = savedInstanceState.getInt("splitBy");
+		TextView splitBy = (TextView) findViewById(R.id.edit_splitby);
+		splitBy.setText(String.format("%02d", splitValue));
+		
+		EditText billTotal = (EditText) findViewById(R.id.total_value);
+		BigDecimal total = new BigDecimal(billTotal.getText().toString());
+		
+		SplitBill(splitValue, total);
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		// TODO Auto-generated method stub
+		super.onSaveInstanceState(outState);
+		
+		TextView splitBy = (TextView) findViewById(R.id.edit_splitby);
+		int split = Integer.parseInt(splitBy.getText().toString());
+		outState.putInt("splitBy", split);
+	}
+
 	public void splitMinus(View view) {
 		try {
 			TextView splitBy = (TextView) findViewById(R.id.edit_splitby);
@@ -40,7 +63,10 @@ public class MainActivity extends Activity {
 
 			splitBy.setText(String.format("%02d", split));
 
-			SplitBill(split);
+			EditText billTotal = (EditText) findViewById(R.id.total_value);
+			BigDecimal total = new BigDecimal(billTotal.getText().toString());
+			
+			SplitBill(split, total);			
 		} catch (NumberFormatException nfe) {
 			System.out.println("Could not parse " + nfe);
 		}
@@ -51,21 +77,25 @@ public class MainActivity extends Activity {
 			TextView splitBy = (TextView) findViewById(R.id.edit_splitby);
 			int split = Integer.parseInt(splitBy.getText().toString());
 
-			if (split < 50) {
+			if (split < 100) {
 				split++;
 			}
 
 			splitBy.setText(String.format("%02d", split));
-			SplitBill(split);
+			
+			EditText billTotal = (EditText) findViewById(R.id.total_value);
+			BigDecimal total = new BigDecimal(billTotal.getText().toString());
+			
+			SplitBill(split, total);
 		} catch (NumberFormatException nfe) {
 			System.out.println("Could not parse " + nfe);
 		}
 	}
 
-	private void SplitBill(int split) {
+	private void SplitBill(int split, BigDecimal total) {
 		final SplitBillAdapter adapter;
 		final ListView listview = (ListView) findViewById(R.id.listview_bills);
-		final EditText billTotal = (EditText) findViewById(R.id.edit_message);
+		
 		ArrayList<BillItem> billList = new ArrayList<BillItem>();
 
 		if (split <= 0) {
@@ -73,12 +103,9 @@ public class MainActivity extends Activity {
 			adapter = new SplitBillAdapter(this, billList);
 			listview.setAdapter(adapter);
 			return;
-		}
+		}		
 
-		BigDecimal total = new BigDecimal(billTotal.getText().toString());
-
-		Money money = MoneyMaker.makeMoney(Currency.getInstance(Locale.UK),
-				total);
+		Money money = MoneyMaker.makeMoney(Currency.getInstance(Locale.UK),	total);
 		Money[] bills = money.proRate(split);
 
 		int index = 0;
